f445eea2d13941eca0d6a01e2d58d17d571b0dff
Esneyder110 <Esneyder110@gmail.com>
2019-03-20 14:14:48 -0400

    no error jaja

--- ejer2/main.cpp
+++ ejer2/main.cpp
@@ -1,103 +1,149 @@
+
 // Author: Esneyder Monsalve
-// Buildin Height
+// Regalos
 
 #include <iostream>
 #include <cmath>
 #include <cctype>
+#include <string>
+
+#define MAX 1200
 
 using namespace std;
 
-double methodOne(double*, double*);
-double readPossitive();
-double methodTwo(double*, double*, double*);
-double toRadian(double);
-
-
-int main()
+struct Gift
 {
-	char option;
-	bool optionNoOk;
-	double height;
+    string genero;
+    int edad;
+    char envoltura;
+};
 
-	// Method One
-	double base, quantity;
+struct Student
+{
+    int cedula;
+    string asignatura;
+    int numeroSeccion;
+    Gift regalo;
+};
 
-	// Method Two
-	double horizontalDistance, eyeHeight, alpha;
+
+void carga(Student*, int*);
+char sino();
+int porceEnvoltura(Student Estudiantes[], int tam, double *porce);
+
+int main() // int argc, char const *argv[]
+{   
+  Student Estudiantes[MAX];
+  // A
+  int numeroAlumnosRegalo = 0;
+  // B
+  int sinenv;
+  double porce;
+ 
 
 
-	do
-	{
-		cout << "A-) Metodo por bloques repetitivos \n";
-		cout << "B-) Metodo por trigonometria\n";
-		cin >> option;
-		option = toupper(option);
+  carga(Estudiantes, &numeroAlumnosRegalo);
+  sinenv = porceEnvoltura(Estudiantes, numeroAlumnosRegalo, &porce);
+  cout << "El porcentaje de regalos para nina es: " << porce << "\%" << endl;
+  cout << "La cantidad de regalos sin envoltura es: " << sinenv << endl;
 
-		optionNoOk = ((option != 'A') && (option != 'B'));
-		
-		if (optionNoOk)
-			cout << "Opcion invalida\n";	
-		
-	}while( optionNoOk );
 
-	if (option == 'A')
-	{
-		cout << "Ingrese el tamano de la unidad base (bloque/nivel): ";
-		base = readPossitive();
-		cout << "Ingrese la cantidad de (bloque/niveles): ";
-		quantity = readPossitive();
-
-	
-		height = methodOne(&base, &quantity);
-	}
-	else
-	{
-		cout << "Ingrese la distancia horizontal: ";
-		horizontalDistance = readPossitive();
-		cout << "Ingrese la altura de los ojos: ";
-		eyeHeight = readPossitive();
-		cout << "Ingrese el angulo de mirada: ";
-		alpha = readPossitive();
-		
-		height = methodTwo(&horizontalDistance, &eyeHeight, &alpha);
-	}
-	cout << "La altura del edificio es aprox. " << height << endl;
-
-	return 0;
+  return 0;
 }
 
-double methodOne(double* base, double* quantity)
+void carga(Student Estudiantes[], int *contador )
 {
-	return (*base * *quantity);
-}
+	char r;
+  do
+  {
 
-double readPossitive()
-{
-	double n;
-	
-	do
-	{
-		cin >> n;
-		if (n < 0)
+    cout << "Datos del alumno\n";
+    cout << "Ingrese la cedula del Estudiante: ";
+    cin >> Estudiantes[*contador].cedula;
+    cout << "Ingrese la asignatura de interes: ";
+    cin >> Estudiantes[*contador].asignatura;
+    cout << "Ingrese el numero de seccion: ";
+    cin >> Estudiantes[*contador].numeroSeccion;
+    cout << "Caracteristicas del Regalo\n";
+    cout << "Ingrese el genero del regalo (nina/nino): ";
+    cin >> Estudiantes[*contador].regalo.genero;       
+    cout << "Edad comprendida: ";
+    cin >> Estudiantes[*contador].regalo.edad;
+    cout << "Posee envoltura? (S/N)";
+		Estudiantes[*contador].regalo.envoltura = sino();
+
+    *contador += 1;
+		r = 'N';
+
+		if(*contador < MAX)
 		{
-			cout << "Numero negativo invalidos\n";
-			cout << "Intente de nuevo: ";
+			cout << "Desea procesar otro estudiante (S/N)";
+			r = sino();
+    }
+
+  }while((r == 'S') && (*contador < MAX));    
+}
+
+char sino()
+{
+	char r;
+	bool noOk;
+
+	do
+	{
+		cout << ": ";
+		cin >> ws;
+		cin >> r;
+		r = toupper(r);
+		noOk = (r != 'S') && (r != 'N');
+		if (noOk)
+			cout << "Valor invalido intentelo de nuevo\n";
+	}while(noOk);
+
+	return r;
+}
+
+int porceEnvoltura(Student Estudiantes[], int tam, double *porce)
+{
+	int sinenv, ninas;
+
+	for (int i = 0; i < tam; ++i)
+	{
+		if(Estudiantes[i].regalo.genero == "nina")
+		{
+			ninas++;
 		}
-	}while(n < 0);
 
-	return n;
+		switch(Estudiantes[i].regalo.envoltura)
+		{
+			case 'N':
+				sinenv++;
+				break;
+		}
+
+	}
+
+	*porce = (double)ninas / tam * 100.0;
+
+	return sinenv;
 }
 
 
-double methodTwo(double* hd, double* eh, double* alpha )
-{
-	*alpha = toRadian(*alpha);
 
-	return (tan(*alpha) * *hd) + *eh;
-}
 
-double toRadian(double alpha)
-{
-	return alpha * M_PI / 180;
-}
 
+
+
+
+//string toUpper(string str)
+//{
+//    int tam = str.length();
+//    int i = 0;
+//    char *cadena = new char[tam + 1];
+//    for (; i < tam; i++)
+//    {
+//        cadena[i] = toupper(cadena[i]);
+//    }
+//    cadena[i] = '\0';
+//    
+//}
